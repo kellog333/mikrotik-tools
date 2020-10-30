ef9c8381ceb02a60b85e854b022345b3100decec
kellog <kellog@localhost.localdomain>
2020-10-30 15:49:39 -0500

    Added mass filter update

---
+++ mikrotikfilter.py
@@ -0,0 +1,50 @@
+import argparse
+import os
+import sys
+import paramiko
+
+def updateTik(host, user, passw, rules):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    print(f"[*] Connecting to {host}")
+    client.connect(host, username=user, password=passw)
+    print("[*] Adding rules")
+    with open(rules, 'r') as rulefile:
+        rule = rulefile.readlines()
+        for i in rule:
+            i = str(i)
+            stdin, stdout, stderr = client.exec_command(f"ip firewall filter {i}")
+            print(f"[*] Adding {i}")
+    print("[*] Done")
+    print("[*] Closing connection")
+    stdin.close()
+    stdout.close()
+    stderr.close()
+    client.close()
+
+
+def openFile(infile, user, passw, rules):
+    with open(infile, 'r') as filer:
+        hosts = filer.readlines()
+        for i in hosts:
+            i = i.strip()
+            updateTik(i, user, passw, rules)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Mass update firewall filter rules on Mikrotik devices")
+    parser.add_argument('-u', help="Username of devices", dest='username')
+    parser.add_argument('-p', help="Password of devices", dest='password')
+    parser.add_argument('-t', help="List of Mikrotik devices", dest='tiks')
+    parser.add_argument('-r', help="List of rules", dest='rules')
+    args = parser.parse_args()
+    user = args.username
+    passw = args.password
+    infile = args.tiks
+    rules = args.rules
+    openFile(infile, user, passw, rules)
+
+
+if __name__ == "__main__":
+    main()
